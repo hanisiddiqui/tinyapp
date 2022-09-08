d87a8dfa5bfb28cff6a72388def5f45d06ed1eea
Hani Siddiqui <hani.siddiqui@gmail.com>
2022-09-08 00:55:30 +0000

    Added login & logout feature with cookies

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require('cookie-parser');
 
 let longURL = "";
 
@@ -17,6 +18,7 @@ const generateRandomString = () => {
 }
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -35,30 +37,44 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const username = req.cookies.username || null;
+  const templateVars = { urls: urlDatabase, username};
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const username = req.cookies.username || null;
+  const templateVars = { username };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const username = req.cookies.username || null;
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username };
   res.render("urls_show", templateVars);
+});
+
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
 });
 
 app.post("/urls/:id", (req, res) => {
   longURL = req.body['longURL'];
   const shortURL = req.params.id;
   urlDatabase[shortURL] = longURL;
-  res.redirect('/urls');
+  res.redirect("/urls");
 });
 
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
-  res.redirect('/urls');
-})
+  res.redirect("/urls");
+});
 
 app.post("/urls", (req, res) => {
   longURL = req.body['longURL'];
